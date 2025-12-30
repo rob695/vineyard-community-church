9655a90a10c5f5436d4a78b0f324441e433843c4
Rob Gee <rob@vineyardcommunity.org.uk>
2025-12-30 14:02:12 +0000

    Manual code change

--- src/components/home/ContactSection.jsx
+++ src/components/home/ContactSection.jsx
@@ -160,8 +160,9 @@ export default function ContactSection() {
             {/* Map Placeholder */}
             <div className="rounded-2xl overflow-hidden shadow-lg h-64 bg-gray-200">
               
-                
-                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d744.5073751858496!2d-1.1572502964433249!3d52.257511476893214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2suk!4v1767102773403!5m2!1sen!2suk"
+                                
+              <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d611.8883356871979!2d-1.1571547490826108!3d52.257478132820914!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2suk!4v1767103151913!5m2!1sen!2suk"
+
                 
                 width="100%"
                 height="100%"
