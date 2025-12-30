6b3ee057e5ae8f6218181e3ad75a68c6ca4c5dce
Rob Gee <rob@vineyardcommunity.org.uk>
2025-12-30 15:56:55 +0000

    Manual code change

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -210,8 +210,14 @@ export default function About() {
           </motion.div>
 
           <div className="grid sm:grid-cols-1 max-w-4xl mx-auto">
-                          <p className="text-gray-600">
+              <p className="text-gray-600">
                 We are here in Daventry to gather and grow “Everyday people who are called to extend God’s Kingdom, love and life to our communities by following the person and the ‘Way’ of Jesus Christ”
+              </p>
+              <p className="text-gray-600">
+                While he was on the earth, Jesus established his Church - a band of followers who would participate in his mission. And the beautiful thing is, he didn't go out to select those who are the strongest, the fastest and the most professional to play their part in that. He chose the weak and the weary; the last, the least and the lost. Its' the faceless nobodies who God partners with in order to outwork his mission for this world. Defining his mission, Jesus said that God had sent him in to this world to bring Good News to the poor, freedom for the captives, healing for the sick and relief for those who are oppressed.
+              </p>
+              <p className="text-gray-600">
+                So, we believe that our purpose is to embody that message in the context of contemporary culture; to those who live around us. In essence, we are not only to be communicators but also demonstrators of God’s limitless love and grace to the world.
               </p>
           </div>
         </div>
