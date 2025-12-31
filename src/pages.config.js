18d5c9a68cf54e2e405b5458cd5d81bcbcfb1d1f
Rob Gee <rob@vineyardcommunity.org.uk>
2025-12-31 22:41:48 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,7 +1,6 @@
 import About from './pages/About';
 import CommunityOutreach from './pages/CommunityOutreach';
 import Contact from './pages/Contact';
-import Events from './pages/Events';
 import Home from './pages/Home';
 import KindnessProjects from './pages/KindnessProjects';
 import LifeGroups from './pages/LifeGroups';
@@ -9,6 +8,7 @@ import ListenAgain from './pages/ListenAgain';
 import NewcomersLunch from './pages/NewcomersLunch';
 import Sunday from './pages/Sunday';
 import Team from './pages/Team';
+import Events from './pages/Events';
 import __Layout from './Layout.jsx';
 
 
@@ -16,7 +16,6 @@ export const PAGES = {
     "About": About,
     "CommunityOutreach": CommunityOutreach,
     "Contact": Contact,
-    "Events": Events,
     "Home": Home,
     "KindnessProjects": KindnessProjects,
     "LifeGroups": LifeGroups,
@@ -24,6 +23,7 @@ export const PAGES = {
     "NewcomersLunch": NewcomersLunch,
     "Sunday": Sunday,
     "Team": Team,
+    "Events": Events,
 }
 
 export const pagesConfig = {
